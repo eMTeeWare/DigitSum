70c4cc06f83527c27618b92bfbf2f95b74a84dc0
Merlin Thomas <dev@emteeware.net>
2015-11-08 13:59:00 +0100

    method stub for getDigitSum

---
+++ src/main/java/DigitSum.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Merlin Thomas on 2015-11-08.
+ * Copyright 2015 eMTeeWare
+ */
+public class DigitSum {
+    /**
+     * @param input
+     * An integer you want to calculate the sum of digits for.
+     * @return
+     * The sum of digits for the provided input.
+     */
+    public static int getDigitSum(int input) {
+        return 0;
+    }
+}
